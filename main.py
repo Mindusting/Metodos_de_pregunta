adbca21dc4122a6287a8153a9f3a9c92baeaec53
AlainG181 <128253784+Mindusting@users.noreply.github.com>
2023-08-16 16:49:33 +0200

    Se reorganizan los arhcivos

--- main.py
+++ main.py
@@ -1,79 +1,9 @@
-# Método de pregunta con respuesta de tipo int.
-def ask_int(text):
+# Se importa la clase "ask"
+import ask
 
-    while True:
-        try:
-            # Intenta convertir el input a int
+# Se guardan los valores de "X" e "Y"
+x = ask.ask_float("Value of X")
+y = ask.ask_float("Value of Y")
 
-            return int(input(f"{text}: "))
-        
-        except ValueError:
-            # En caso de tipo de excepción
-            # de tipo "ValueError", se le
-            # indica al usuario lo sucedido.
-
-            print("The value is not of type int.")
-            print("Try again...")
-        
-        except:
-            # En caso de excepción genérica
-            # se le indica al usuario lo sucedido.
-
-            print("The value is not valid.")
-            print("Try again...")
-
-
-# Método de pregunta con respuesta de tipo float.
-def ask_float(text):
-
-    while True:
-        try:
-            # Intenta convertir el input a float
-
-            return float(input(f"{text}: "))
-        
-        except ValueError:
-            # En caso de tipo de excepción
-            # de tipo "ValueError", se le
-            # indica al usuario lo sucedido.
-
-            print("The value is not of type float.")
-            print("Try again...")
-        
-        except:
-            # En caso de excepción genérica
-            # se le indica al usuario lo sucedido.
-
-            print("The value is not valid.")
-            print("Try again...")
-
-
-# Método de pregunta con respuesta de tipo bool.
-def ask_bool(text):
-
-    while True:
-
-        # Guarda la respuesta del usuario
-        res = input(f"{text} [Y/N]: ")
-
-        if res.upper() == "Y" or res == "1":
-
-            # Si la respuesta es "Y"
-            # o "1" devuelve True.
-
-            return True
-        
-        elif res.upper() == "N" or res == "0":
-
-            # Si la respuesta es "N"
-            # o "0" devuelve False.
-
-            return False
-        
-        else:
-            # Si no es ninguna de las
-            # anteriores, el valor
-            # no es válido.
-
-            print("The value is not of type bool.")
-            print("Try again...")
+# Se opera con los valores de X" e "Y"
+print(f"{x} + {y} = {x + y}")
