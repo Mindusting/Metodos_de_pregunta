87ca4ca540612311dc671189cd993e5a99f227fb
AlainG181 <128253784+Mindusting@users.noreply.github.com>
2023-08-16 16:39:44 +0200

    Ask_bool

--- main.py
+++ main.py
@@ -46,3 +46,35 @@ def ask_float(text):
 
             print("The value is not valid.")
             print("Try again...")
+
+
+# Método de pregunta con respuesta de tipo bool.
+def ask_bool(text):
+
+    while True:
+
+        # Guarda la respuesta del usuario
+        res = input(f"{text} [Y/N]: ")
+
+        if res.upper() == "Y" or res == "1":
+
+            # Si la respuesta es "Y"
+            # o "1" devuelve True.
+
+            return True
+        
+        elif res.upper() == "N" or res == "0":
+
+            # Si la respuesta es "N"
+            # o "0" devuelve False.
+
+            return False
+        
+        else:
+
+            # Si no es ninguna de las
+            # anteriores, el valor
+            # no es válido.
+
+            print("The value is not of type bool.")
+            print("Try again...")
